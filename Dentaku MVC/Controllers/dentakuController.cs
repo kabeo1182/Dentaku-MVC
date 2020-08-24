130b216d2d5ad5a04ec3a781856c9ea562ea647a
KABEO-PC\81903 <g.okabe.business@gmail.com>
2020-08-24 17:57:13 +0900

    四則演算の処理を考え中。普通の電卓と少しタイプが違うので悩み中。

--- Dentaku MVC/Controllers/dentakuController.cs
+++ Dentaku MVC/Controllers/dentakuController.cs
@@ -54,6 +54,57 @@ namespace Dentaku_MVC.Controllers
                        
                         switch (kari)
                         {
+                            case "+":
+                                break;
+
+                            case "-":
+                                break;
+
+                            case "/":
+                                break;
+
+                            case "*":
+                                break;
+
+                            case "%":
+                                break;
+
+                            case "(":
+                                break;
+
+                            case ")":
+                                break;
+
+                            case "0":
+                                break;
+
+                            case "1":
+                                break;
+
+                            case "2":
+                                break;
+
+                            case "3":
+                                break;
+
+                            case "4":
+                                break;
+
+                            case "5":
+                                break;
+
+                            case "6":
+                                break;
+
+                            case "7":
+                                break;
+
+                            case "8":
+                                break;
+
+                            case "9":
+                                break;
+
                             default:
                                 break;
                         }
