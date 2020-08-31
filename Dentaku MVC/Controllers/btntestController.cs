faafcd8e20986bb9c94f8c44a36859b5e20f0665
KABEO-PC\81903 <g.okabe.business@gmail.com>
2020-08-31 17:43:59 +0900

    ボタンごとに処理を分岐できるページを作成。

---
+++ Dentaku MVC/Controllers/btntestController.cs
@@ -0,0 +1,245 @@
+﻿using Antlr.Runtime;
+using Dentaku_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.Web.Mvc;
+
+namespace Dentaku_MVC.Controllers
+{
+    public class btntestController : Controller
+    {
+        // GET: btntest
+        public ActionResult btntest(btntestModel model,string keisan)
+        {
+            if (keisan == "+")
+            {
+               model.result = CalcProcPlus(model).ToString();
+            }
+            else if (keisan == "-")
+            {
+                model.result = CalcProcMinus(model).ToString();
+            }
+            else if (keisan == "*")
+            {
+                model.result = CalcProcTimesMultiplied(model).ToString();
+            }
+            else if (keisan == "/")
+            {
+                model.result = CalcProcDivided(model).ToString();
+            }
+            return View(model);
+        }
+
+        private int CalcProcPlus(btntestModel model)
+        {
+
+            try
+            {
+                string left = String.Empty;
+                string right = String.Empty;
+                int leftres = 0;
+                int rightres = 0;
+                int result = 0;
+
+                //値渡しをする
+                left = model.leftsiki;
+                right = model.rightsiki;
+
+                //左の式が空の時は処理を抜ける
+                if (String.IsNullOrEmpty(left))
+                {
+                    return 0;
+                }
+                //右の式が空の時は処理を抜ける
+                if (String.IsNullOrEmpty(right))
+                {
+                    return 0;
+                }
+
+                //数字に変換可能か
+                if (!int.TryParse(left, out leftres))
+                {
+                    //メッセージボックス（アラート）使いたいけど、JavaScriptの力が必要。
+                    //とりあえず後回し
+                    return 0;
+                }
+                //数字に変換可能か
+                if (!int.TryParse(right, out rightres))
+                {
+                    //メッセージボックス（アラート）使いたいけど、JavaScriptの力が必要。
+                    //とりあえず後回し
+                    return 0;
+                }
+
+                result = leftres + rightres;
+                       
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        private int CalcProcMinus(btntestModel model)
+        {
+
+            try
+            {
+                string left = String.Empty;
+                string right = String.Empty;
+                int leftres = 0;
+                int rightres = 0;
+                int result = 0;
+
+                //値渡しをする
+                left = model.leftsiki;
+                right = model.rightsiki;
+
+                //左の式が空の時は処理を抜ける
+                if (String.IsNullOrEmpty(left))
+                {
+                    return 0;
+                }
+                //右の式が空の時は処理を抜ける
+                if (String.IsNullOrEmpty(right))
+                {
+                    return 0;
+                }
+
+                //数字に変換可能か
+                if (!int.TryParse(left, out leftres))
+                {
+                    //メッセージボックス（アラート）使いたいけど、JavaScriptの力が必要。
+                    //とりあえず後回し
+                    return 0;
+                }
+                //数字に変換可能か
+                if (!int.TryParse(right, out rightres))
+                {
+                    //メッセージボックス（アラート）使いたいけど、JavaScriptの力が必要。
+                    //とりあえず後回し
+                    return 0;
+                }
+
+                result = leftres - rightres;
+
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        private int CalcProcTimesMultiplied(btntestModel model)
+        {
+
+            try
+            {
+                string left = String.Empty;
+                string right = String.Empty;
+                int leftres = 0;
+                int rightres = 0;
+                int result = 0;
+
+                //値渡しをする
+                left = model.leftsiki;
+                right = model.rightsiki;
+
+                //左の式が空の時は処理を抜ける
+                if (String.IsNullOrEmpty(left))
+                {
+                    return 0;
+                }
+                //右の式が空の時は処理を抜ける
+                if (String.IsNullOrEmpty(right))
+                {
+                    return 0;
+                }
+
+                //数字に変換可能か
+                if (!int.TryParse(left, out leftres))
+                {
+                    //メッセージボックス（アラート）使いたいけど、JavaScriptの力が必要。
+                    //とりあえず後回し
+                    return 0;
+                }
+                //数字に変換可能か
+                if (!int.TryParse(right, out rightres))
+                {
+                    //メッセージボックス（アラート）使いたいけど、JavaScriptの力が必要。
+                    //とりあえず後回し
+                    return 0;
+                }
+
+                result = leftres * rightres;
+
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        private int CalcProcDivided(btntestModel model)
+        {
+
+            try
+            {
+                string left = String.Empty;
+                string right = String.Empty;
+                int leftres = 0;
+                int rightres = 0;
+                int result = 0;
+
+                //値渡しをする
+                left = model.leftsiki;
+                right = model.rightsiki;
+
+                //左の式が空の時は処理を抜ける
+                if (String.IsNullOrEmpty(left))
+                {
+                    return 0;
+                }
+                //右の式が空の時は処理を抜ける
+                if (String.IsNullOrEmpty(right))
+                {
+                    return 0;
+                }
+
+                //数字に変換可能か
+                if (!int.TryParse(left, out leftres))
+                {
+                    //メッセージボックス（アラート）使いたいけど、JavaScriptの力が必要。
+                    //とりあえず後回し
+                    return 0;
+                }
+                //数字に変換可能か
+                if (!int.TryParse(right, out rightres))
+                {
+                    //メッセージボックス（アラート）使いたいけど、JavaScriptの力が必要。
+                    //とりあえず後回し
+                    return 0;
+                }
+
+                result = leftres / rightres;
+
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ Dentaku MVC/Models/btntestModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dentaku_MVC.Models
+{
+    public class btntestModel
+    {
+        [Display(Name = "計算式1")]
+        [RegularExpression(@"[0-9]+", ErrorMessage = "半角数字のみ入力できます")]
+        public string leftsiki { get; set; }
+
+        [Display(Name = "計算式2")]
+        [RegularExpression(@"[0-9]+", ErrorMessage = "半角数字のみ入力できます")]
+        public string rightsiki { get; set; }
+
+        [Display(Name = "答え")]
+        public string result { get; set; }
+    }
+}
